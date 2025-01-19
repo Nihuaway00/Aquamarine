73d2f23d9d9e331f0abbbf6e3ff4268050416bd1
Nihuaway00 <nikitaemaeny@gmail.com>
2025-01-19 18:49:31 +0300

    [small fix] переименовал функцию сохранения файла

--- src/backend/executor/src/minio/minio.service.ts
+++ src/backend/executor/src/minio/minio.service.ts
@@ -8,7 +8,7 @@ export class MinioService {
 	constructor(@Inject(MINIO_CONNECTION) private readonly minioClient: Client) {
 	}
 
-	async uploadStream(bucket: string, filename: string, buffer: Uint8Array) {
+	async uploadBuffer(bucket: string, filename: string, buffer: Uint8Array) {
 		const bufferStream = new Readable();
 		bufferStream.push(buffer);
 		bufferStream.push(null);
