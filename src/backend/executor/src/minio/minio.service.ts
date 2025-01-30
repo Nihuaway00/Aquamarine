cf1d3f72dd272829bcb392dd2faf77620d3ea790
Nihuaway00 <nikitaemaeny@gmail.com>
2025-01-30 16:33:56 +0300

    [small feature] добавлено удаление файлов через день

--- src/backend/executor/src/minio/minio.service.ts
+++ src/backend/executor/src/minio/minio.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Readable } from 'stream';
 import { ConfigService } from '@nestjs/config';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
@@ -7,13 +7,17 @@ import { InjectMinio } from '../decorators/minio.decorator';
 import * as Minio from 'minio';
 
 @Injectable()
-export class MinioService {
+export class MinioService implements OnModuleInit {
 	defaultBucket: string;
 
 	constructor(@InjectMinio() private readonly client: Minio.Client,
 							configService: ConfigService,
 							@InjectPinoLogger(AppService.name) private readonly logger: PinoLogger) {
 		this.defaultBucket = configService.get('MINIO_DEFAULT_BUCKETS').split(',')[0];
+	}
+
+	async onModuleInit() {
+		await this.setLifecycle(this.defaultBucket, 1);
 	}
 
 	async uploadBuffer(filename: string, buffer: Uint8Array) {
@@ -32,5 +36,21 @@ export class MinioService {
 
 	async getDownloadUrl(filename: string, expires: number = 3600) {
 		return await this.client.presignedUrl('GET', this.defaultBucket, filename, expires);
+	}
+
+	async setLifecycle(bucket: string, days: number) {
+		const lifecycleConfig = {
+			Rule: [
+				{
+					ID: `AutoDeleteAfter${days}Days`,
+					Status: 'Enabled',
+					Prefix: '',
+					Expiration: { Days: days },
+				},
+			],
+		};
+		// @ts-ignore
+		await this.client.setBucketLifecycle(bucket, lifecycleConfig);
+		return { message: `Lifecycle set: delete files after ${days} days` };
 	}
 }
